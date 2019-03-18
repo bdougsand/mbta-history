6b01802f8796cafd276c4d00afda9caaf1661862
Brian Sanders <bdougsand+cfb@gmail.com>
2019-03-18 08:19:15 -0400

    Example configuration file

---
+++ config.example.py
@@ -0,0 +1,9 @@
+AWSParams = {
+}
+
+TwitterCredentials = {
+    "consumer_key": "<your consumer_key here>",
+    "consumer_secret": "<your consumer_secret here>",
+    "access_token": "<your access_token here>",
+    "access_token_secret": "<your access_token_secret here>"
+}
